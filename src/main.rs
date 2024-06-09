0abae4378f75ae05858674fee3466a10e0dc7cdf
Lucas Leonardo <lucasleonardo@protonmail.com>
2024-06-09 03:09:24 -0300

    misc: first image load omg

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![feature(iterator_try_collect)]
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
