d5b87ec66f39a852e2a9ba6e851da73442040e73
Lucas Leonardo <lucasleonardo@protonmail.com>
2024-06-17 20:50:05 -0300

    texture: add new file and comments

---
+++ src/texture.rs
@@ -0,0 +1,112 @@
+use eframe::wgpu;
+use image::GenericImageView;
+use std::cmp;
+
+/// A representation of an image in the GPU
+pub struct Texture {
+    /// A handle to the data
+    pub tex: wgpu::Texture,
+
+    /// A way to view into said data
+    pub view: wgpu::TextureView,
+
+    /// A way to fetch the data, like a single texel
+    pub sampler: wgpu::Sampler,
+
+    /// The dimensions
+    pub size: (u32, u32),
+}
+
+impl Texture {
+    /// Creates a texture from a parsed image
+    pub fn from_image(
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        img: &image::DynamicImage,
+    ) -> Self {
+        let (width, height) = img.dimensions();
+        let bytes_per_pixel = std::mem::size_of::<image::Rgb<u16>>();
+        let bytes = img.to_rgba8();
+        let size = wgpu::Extent3d {
+            width,
+            height,
+            depth_or_array_layers: 1,
+        };
+
+        let tex = device.create_texture(&wgpu::TextureDescriptor {
+            label: Some("image"),
+            size,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb, // TODO: dynamically infer this
+            usage: wgpu::TextureUsages::TEXTURE_BINDING
+                | wgpu::TextureUsages::COPY_DST
+                | wgpu::TextureUsages::RENDER_ATTACHMENT,
+            view_formats: &[],
+            mip_level_count: (cmp::max(width, height).ilog2() + 1).min(10),
+        });
+
+        queue.write_texture(
+            wgpu::ImageCopyTexture {
+                texture: &tex,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+                aspect: wgpu::TextureAspect::All,
+            },
+            &bytes,
+            wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: Some((width as usize * bytes_per_pixel) as u32),
+                rows_per_image: Some(height),
+            },
+            size,
+        );
+
+        let view = tex.create_view(&wgpu::TextureViewDescriptor::default());
+
+        let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+            label: Some("Texture sampler descriptor"),
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+
+            // Helps make a smoother, more natural zoom in/out
+            mag_filter: wgpu::FilterMode::Nearest,
+            min_filter: wgpu::FilterMode::Linear,
+            mipmap_filter: wgpu::FilterMode::Linear,
+
+            ..Default::default()
+        });
+
+        Self {
+            tex,
+            view,
+            sampler,
+            size: (width, height),
+        }
+    }
+
+    pub fn get_bind_group_layout(device: &wgpu::Device) -> wgpu::BindGroupLayout {
+        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("Texture bind group layout"),
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Texture {
+                        multisampled: false,
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                        sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                    count: None,
+                },
+            ],
+        })
+    }
+}
