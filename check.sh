2d0baf8a65b456511450d79979bb42dd32c393f9
Lucas Leonardo <lucasleonardo@protonmail.com>
2024-06-25 15:52:35 -0300

    ci/cd: remove obsolete check

--- check.sh
+++ check.sh
@@ -6,4 +6,3 @@ cargo check --quiet --workspace --all-targets
 cargo fmt --all -- --check
 cargo clippy --quiet --workspace --all-targets --all-features --  -D warnings -W clippy::all
 cargo test --quiet --workspace --all-targets --all-features
-cargo test --quiet --workspace --doc
